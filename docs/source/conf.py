ae7f8c968b5159fd3fa7768d573bc6ea50955096
sergiocalde94 <sergiocalde94@gmail.com>
2020-05-21 19:25:06 +0200

    Data And Model Drift Checker Version 0.1.7 (beta)
    - New class `DriftCheckerEstimator` for make pydrift working as sklearn estimator (for pipelines for example)
    - Changed `auc_threshold` in `ml_model_can_discriminate` to a less restrictive one
    - New examples at notebooks folder
    - Minor changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Sergio Calderón Pérez-Lozao'
 author = 'Sergio Calderón Pérez-Lozao'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.6'
+release = '0.1.7'
 
 
 # -- General configuration ---------------------------------------------------
--- pydrift/__init__.py
+++ pydrift/__init__.py
@@ -2,3 +2,4 @@ from .core.drift_checker import (DriftChecker,
                                  DataDriftChecker,
                                  ModelDriftChecker)
 from .core.interpretable_drift import InterpretableDrift
+from .core.drift_checker_estimator import DriftCheckerEstimator
